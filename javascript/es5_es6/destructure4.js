caba0ed853ce01e8b3af46ede7ccd1935d46bef4
Bxsss09 <thapapat2000@gmail.com>
2024-08-04 18:20:03 +0700

    Create destructure4.js

---
+++ javascript/es5_es6/destructure4.js
@@ -0,0 +1,10 @@
+const person = {
+    first: "saha"
+    last: "Michel"
+    email:"thapapt2000@gmail.com"
+    birthday: {
+        day: 1,
+        month: {Jan}
+        year: 2024
+    }    
+};
